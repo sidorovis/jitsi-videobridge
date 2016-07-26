52ddab75ff66a0f894c40bb51c2218178231a69a
paweldomas <pawel.domas@jitsi.org>
2016-07-26 15:46:24 -0500

    Add "SCTP connection ready" event

--- src/main/java/org/jitsi/videobridge/Conference.java
+++ src/main/java/org/jitsi/videobridge/Conference.java
@@ -1407,6 +1407,12 @@ public class Conference
                  * implement it as well.
                  */
                 endpoint.sctpConnectionReady(sctpConnection);
+                // Trigger SCTP connection ready event
+                if (eventAdmin != null)
+                {
+                    eventAdmin.postEvent(
+                            EventFactory.endpointSctpConnReady(endpoint));
+                }
             }
         }
     }
